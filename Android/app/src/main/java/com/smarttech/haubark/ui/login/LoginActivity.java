f77cc67538f1189470b668bc8737af5e6f099c51
Dmitry Abakumov <killerinshadow2@gmail.com>
2020-08-22 12:22:16 +0300

    Add login screen
    [re #1]

---
+++ Android/app/src/main/java/com/smarttech/haubark/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.smarttech.haubark.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smarttech.haubark.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
