af06dc00b6f6247e177c8143eb6bf8086327abef
Dmitry Abakumov <killerinshadow2@gmail.com>
2020-08-22 19:49:38 +0300

    WIP: Change color scheme, add move to main
    [re #1]

--- Android/app/src/main/java/com/smarttech/haubark/ui/main/MainActivity.java
+++ Android/app/src/main/java/com/smarttech/haubark/ui/main/MainActivity.java
@@ -2,14 +2,10 @@ package com.smarttech.haubark.ui.main;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 
 import com.smarttech.haubark.R;
 
@@ -18,23 +14,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+        setupTabsIcons(tabs);
+    }
 
+    private void setupTabsIcons(TabLayout tabs) {
         tabs.getTabAt(0).setIcon(R.drawable.building);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        tabs.getTabAt(1).setIcon(R.drawable.ic_appeal);
+        tabs.getTabAt(2).setIcon(R.drawable.ic_voting);
     }
 }
